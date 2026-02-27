b964b0e473a5c868125bfa16f805dfd295d1a2e4
Tabeer Shahjahan <125653653+technifai@users.noreply.github.com>
2026-02-27 14:27:41 +0530

    Create protect.js

---
+++ js/protect.js
@@ -0,0 +1,7 @@
+import { auth } from "./firebase-config.js";
+import { onAuthStateChanged }
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+onAuthStateChanged(auth, user => {
+  if (!user) window.location.href = "login.html";
+});
